1e2ca22355a72534122ba12049502b779ebcd239
Paul Parks <paul@smartsam.com>
2022-10-15 04:25:51 +0800

    Fixed by_name functions

--- CodePypes/__init__.py
+++ CodePypes/__init__.py
@@ -6,12 +6,12 @@ from .core import get_api
 from .identity import get_profile
 from .identity import get_self
 from .identity import get_organizations
-from .identity import get_organizations_by_id
-from .identity import get_organization_by_id
+from .identity import get_organization
 from .identity import get_organizations_by_name
 from .identity import get_organization_by_name
 from .identity import get_credentials
 from .identity import get_projects
+from .identity import get_project
 from .identity import get_projects_by_name
 from .identity import get_project_by_name
 
--- CodePypes/identity.py
+++ CodePypes/identity.py
@@ -13,6 +13,9 @@ def get_self():
 def get_organizations():
     return get_api("identity", "organizations")
 
+def get_organization(id):
+    return get_api("identity", "organizations/{id}".format(id=id))
+
 def get_organizations_by_name():
     orgs = {}
     rsp = get_organizations()
@@ -26,24 +29,14 @@ def get_organization_by_name(name):
     orgs = get_organizations_by_name()
     return orgs[name]
 
-def get_organizations_by_id():
-    orgs = {}
-    rsp = get_organizations()
-
-    if rsp['status_code'] == 200:
-        orgs = make_dict(rsp['response']['orgs'], 'id')
-
-    return orgs
-
-def get_organization_by_id(id):
-    orgs = get_organizations_by_id()
-    return orgs[id]
-
 def get_credentials(organization_id):
     return get_api("identity", "organizations/{id}/creds".format(id=organization_id))
 
 def get_projects(organization_id):
     return get_api("identity", "organizations/{id}/projects".format(id=organization_id))
+
+def get_project(organization_id, project_id):
+    return get_api("identity", "organizations/{organization_id}/projects/{project_id}".format(organization_id=organization_id,project_id=project_id))
 
 def get_projects_by_name(organization_id):
     projects = {}
